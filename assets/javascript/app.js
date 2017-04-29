490994979e275a2c914b408f8b00759726ab5df4
Tori Dean <vdean2011@gmail.com>
2017-04-29 09:50:50 -0500

    removed old map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -126,29 +126,29 @@ dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functio
 });
 
 
-     function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: {lat: 29.7604, lng: -95.3698}
-        });
-        var geocoder = new google.maps.Geocoder();
+//      function initMap() {
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 12,
+//           center: {lat: 29.7604, lng: -95.3698}
+//         });
+//         var geocoder = new google.maps.Geocoder();
 
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
+//         document.getElementById('submit').addEventListener('click', function() {
+//           geocodeAddress(geocoder, map);
+//         });
+//       }
 
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
+//       function geocodeAddress(geocoder, resultsMap) {
+//         var address = document.getElementById('address').value;
+//         geocoder.geocode({'address': address}, function(results, status) {
+//           if (status === 'OK') {
+//             resultsMap.setCenter(results[0].geometry.location);
+//             var marker = new google.maps.Marker({
+//               map: resultsMap,
+//               position: results[0].geometry.location
+//             });
+//           } else {
+//             alert('Geocode was not successful for the following reason: ' + status);
+//           }
+//         });
+//       }
